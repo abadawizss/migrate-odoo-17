87668cd6da95b2eacd5ac3fc2c447239ad58e41a
Ahmad Badawi <ahmad.badawi@zensoftware.us>
2024-01-30 01:52:47 +0700

    minor fixing

--- zen_hr_attendance_extended/models/hr_work_location.py
+++ zen_hr_attendance_extended/models/hr_work_location.py
@@ -4,6 +4,6 @@ from odoo import models, fields
 class HrWorkLocation(models.Model):
     _inherit = 'hr.work.location'
 
-    lat = fields.Float(string="Lattitude", digits=(10, 7), readonly=True)
-    long = fields.Float(string="Longitude", digits=(10, 7), readonly=True)
+    lat = fields.Float(string="Lattitude", digits=(10, 7))
+    long = fields.Float(string="Longitude", digits=(10, 7))
     address = fields.Text(string="Address")

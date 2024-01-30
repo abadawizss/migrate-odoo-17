e159c88e4b24ec0dfb28b4d12376366f35bdfb5c
frakhmanzss <fauzi.rakhman@zensoftware.us>
2024-01-30 10:40:57 +0700

    Add zen_tags_administrator

---
+++ zen_tags_administrator/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Zen Tags Administration',
+    'version': '1.0',
+    'category': 'Zensoftware/Access',
+    'author': "Zen Software Solutions",
+    'summary': 'Zen Tags Administration - v1.0',
+    'website': 'https://www.zensoftwaresolutions.com',
+    'depends': [
+        'crm',
+        'sales_team',
+    ],
+    'data': [
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}

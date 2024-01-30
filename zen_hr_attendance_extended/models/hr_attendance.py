862755703808421010b5d68a7742ea7325ebfbbf
frakhmanzss <fauzi.rakhman@zensoftware.us>
2024-01-30 13:34:47 +0700

    Add get_address_from_maps method to get the location

--- zen_hr_attendance_extended/models/hr_attendance.py
+++ zen_hr_attendance_extended/models/hr_attendance.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from geopy.geocoders import Nominatim
 
 
 class HrAttendance(models.Model):
@@ -8,8 +9,16 @@ class HrAttendance(models.Model):
     check_out_lat = fields.Float(string="Check out Lattitude", digits=(10, 7), readonly=True)
     check_in_long = fields.Float(string="Check in Longitude", digits=(10, 7), readonly=True)
     check_out_long = fields.Float(string="Check out Longitude", digits=(10, 7), readonly=True)
-    address = fields.Text(string="Address")
-    location_link = fields.Text(string="Location Link")
+    address = fields.Text(string="Address", compute='get_address_from_maps', readonly=True)
+    location_link = fields.Html(string="Location Link", widget="url", readonly=True)
     face_image = fields.Image()
     work_area_status_in = fields.Boolean(default=True)
-    work_area_status_out = fields.Boolean()
+    work_area_status_out = fields.Boolean(default=True)
+
+    @api.depends('check_in_lat','check_in_long')
+    def get_address_from_maps(self):
+        geolocator = Nominatim(user_agent='my-app')
+        for rec in self:
+            # rec.address = geolocator.reverse((rec.check_in_lat, rec.check_in_long), language='en').address
+            rec.address = geolocator.reverse(str(rec.check_in_lat) + ', ' + str(rec.check_in_long)).address
+            rec.location_link = f"https://www.google.com/maps/search/?api=1&query={rec.check_in_lat},{rec.check_in_long}"

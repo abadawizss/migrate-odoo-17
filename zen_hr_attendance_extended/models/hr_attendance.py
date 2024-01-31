9aa178ddbbde1d180a42fb476059adf09c4f2a38
frakhmanzss <fauzi.rakhman@zensoftware.us>
2024-01-31 21:13:52 +0700

    Add out_address and out_location_url

--- zen_hr_attendance_extended/models/hr_attendance.py
+++ zen_hr_attendance_extended/models/hr_attendance.py
@@ -9,20 +9,13 @@ class HrAttendance(models.Model):
     check_out_lat = fields.Float(string="Check out Lattitude", digits=(10, 7), readonly=True)
     check_in_long = fields.Float(string="Check in Longitude", digits=(10, 7), readonly=True)
     check_out_long = fields.Float(string="Check out Longitude", digits=(10, 7), readonly=True)
-    address = fields.Text(string="Address", compute='get_address_from_maps')
-    location_link = fields.Char(string="Location Link", readonly=True)
+    address = fields.Text(string="Address", compute='get_address_in_from_maps')
+    out_address = fields.Text(string="Out Address", compute='get_address_out_from_maps')
+    location_url = fields.Char(string="Location url", readonly=True)
+    out_location_url = fields.Char(string="Out Location url", readonly=True)
     face_image = fields.Image()
     work_area_status_in = fields.Boolean(default=True, readonly=True)
     work_area_status_out = fields.Boolean(default=True, readonly=True)
-
-    # @api.depends('check_in_lat','check_in_long')
-    # def get_address_from_maps(self):
-    #     geolocator = Nominatim(user_agent='my-app')
-        # for rec in self:
-        #     if rec.check_in_lat and rec.check_in_long:
-                # rec.address = geolocator.reverse((rec.check_in_lat, rec.check_in_long), language='en').address
-                # rec.address = geolocator.reverse(str(rec.check_in_lat) + ', ' + str(rec.check_in_long)).address
-                # rec.location_link = f"https://www.google.com/maps/search/?api=1&query={rec.check_in_lat},{rec.check_in_long}"
 
     @api.depends('check_in_lat','check_in_long')
     def get_address_from_maps(self):
@@ -32,4 +25,14 @@ class HrAttendance(models.Model):
             if rec.check_in_lat !=0 and rec.check_in_long != 0:
                 rec.address = geolocator.reverse((rec.check_in_lat, rec.check_in_long), language='en').address
                 # rec.address = geolocator.reverse(str(rec.check_in_lat) + ', ' + str(rec.check_in_long)).address
-                rec.location_link = f"https://www.google.com/maps/search/?api=1&query={rec.check_in_lat},{rec.check_in_long}"
+                rec.location_url = f"https://www.google.com/maps/search/?api=1&query={rec.check_in_lat},{rec.check_in_long}"
+
+    @api.depends('check_out_lat','check_out_long')
+    def get_out_address_from_maps(self):
+        geolocator = Nominatim(user_agent='my-app')
+        for rec in self:
+            rec.out_address = ''
+            if rec.check_out_lat !=0 and rec.check_out_long != 0:
+                rec.out_address = geolocator.reverse((rec.check_out_lat, rec.check_out_long), language='en').address
+                # rec.address = geolocator.reverse(str(rec.check_in_lat) + ', ' + str(rec.check_in_long)).address
+                rec.out_location_url = f"https://www.google.com/maps/search/?api=1&query={rec.check_out_lat},{rec.check_out_long}"

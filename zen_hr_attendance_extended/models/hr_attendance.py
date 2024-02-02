e9270cab31ccd98768a40a0425e0065413feb8b3
Ahmad Badawi <ahmad.badawi@zensoftware.us>
2024-02-02 15:30:34 +0700

    minor fixing

--- zen_hr_attendance_extended/models/hr_attendance.py
+++ zen_hr_attendance_extended/models/hr_attendance.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
 from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
 
 
 class HrAttendance(models.Model):
@@ -17,28 +18,32 @@ class HrAttendance(models.Model):
     work_area_status_in = fields.Boolean(default=True, readonly=True)
     work_area_status_out = fields.Boolean(default=True, readonly=True)
 
-    @api.depends('check_in_lat','check_in_long')
+    @api.depends('check_in_lat', 'check_in_long')
     def get_address_from_maps(self):
         geolocator = Nominatim(user_agent='my-app')
         for rec in self:
             rec.address = ''
-            rec.out_location_url = ''
-            if rec.check_in_lat !=0 or rec.check_in_lat != False and rec.check_in_long != 0 or rec.check_in_long != False:
-                address = geolocator.reverse((rec.check_in_lat, rec.check_in_long), language='en').address
-                # rec.address = geolocator.reverse(str(rec.check_in_lat) + ', ' + str(rec.check_in_long)).address
-                if address:
-                    rec.address = address
-                    rec.location_url = f"https://www.google.com/maps/search/?api=1&query={rec.check_in_lat},{rec.check_in_long}"
+            rec.location_url = ''
+            if rec.check_in_lat != 0 and rec.check_in_long != 0:
+                try:
+                    location = geolocator.reverse((rec.check_in_lat, rec.check_in_long), language='en')
+                    if location and location.address:
+                        rec.address = location.address
+                        rec.location_url = f"https://www.google.com/maps/search/?api=1&query={rec.check_in_lat},{rec.check_in_long}"
+                except GeocoderTimedOut:
+                    pass
 
-    @api.depends('check_out_lat','check_out_long')
+    @api.depends('check_out_lat', 'check_out_long')
     def get_address_out_from_maps(self):
         geolocator = Nominatim(user_agent='my-app')
         for rec in self:
             rec.out_address = ''
             rec.out_location_url = ''
-            if rec.check_out_lat !=0 or rec.check_out_lat != False and rec.check_out_long != 0 or rec.check_out_long != False:
-                address = geolocator.reverse((rec.check_out_lat, rec.check_out_long), language='en').address
-                # rec.address = geolocator.reverse(str(rec.check_in_lat) + ', ' + str(rec.check_in_long)).address
-                if address:
-                    rec.address = address
-                    rec.out_location_url = f"https://www.google.com/maps/search/?api=1&query={rec.check_out_lat},{rec.check_out_long}"
+            if rec.check_out_lat != 0 and rec.check_out_long != 0:
+                try:
+                    location = geolocator.reverse((rec.check_out_lat, rec.check_out_long), language='en')
+                    if location and location.address:
+                        rec.out_address = location.address
+                        rec.out_location_url = f"https://www.google.com/maps/search/?api=1&query={rec.check_out_lat},{rec.check_out_long}"
+                except GeocoderTimedOut:
+                    pass

7e5dc13e70eb8fb18c9fb0254a8a6a47512dcb0d
Ahmad Badawi <ahmad.badawi@zensoftware.us>
2024-02-02 15:44:21 +0700

    modul mail template sale

---
+++ zen_mail_template_sale/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Zen Mail Template Sale',
+    'version': '1.0',
+    'category': 'Zensoftware/Mail',
+    'author': "Zen Software Solutions",
+    'summary': 'Zen Mail Template Sale - v1.0',
+    'website': 'https://www.zensoftwaresolutions.com',
+    'depends': [
+        'sale_management',
+        'base',
+    ],
+    'data': [
+        'data/email_template_sale.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}

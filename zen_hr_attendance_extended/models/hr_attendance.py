8d20b2ff22b652b9dfbf45150b1c31f0db26b0cb
frakhmanzss <fauzi.rakhman@zensoftware.us>
2024-02-05 15:14:34 +0700

    Add geotag

--- zen_hr_attendance_extended/models/hr_attendance.py
+++ zen_hr_attendance_extended/models/hr_attendance.py
@@ -10,6 +10,8 @@ class HrAttendance(models.Model):
     check_out_lat = fields.Float(string="Check out Lattitude", default=0.0, digits=(10, 7), readonly=True)
     check_in_long = fields.Float(string="Check in Longitude", default=0.0, digits=(10, 7), readonly=True)
     check_out_long = fields.Float(string="Check out Longitude", default=0.0, digits=(10, 7), readonly=True)
+    geotag = fields.Char("Geotag", compute='_compute_geotag', store=True)
+    out_geotag = fields.Char("Out geotag", compute='_compute_geotag', store=True)
     address = fields.Text(string="Address", compute='get_address_from_maps')
     out_address = fields.Text(string="Out Address", compute='get_address_out_from_maps')
     location_url = fields.Char(string="Location url", readonly=True)
@@ -17,6 +19,12 @@ class HrAttendance(models.Model):
     face_image = fields.Image()
     work_area_status_in = fields.Boolean(default=True, readonly=True)
     work_area_status_out = fields.Boolean(default=True, readonly=True)
+
+    @api.depends('check_in_lat','check_in_long','check_out_lat','check_out_long')
+    def _compute_geotag(self):
+        for rec in self:
+            rec.geotag=f'{rec.check_in_lat},{rec.check_in_long}'
+            rec.out_geotag=f'{rec.check_out_lat},{rec.check_out_long}'
 
     @api.depends('check_in_lat', 'check_in_long')
     def get_address_from_maps(self):

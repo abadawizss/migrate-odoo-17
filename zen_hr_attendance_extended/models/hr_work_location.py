45c099b0fe9be7bd9d308e002006393e44aeb3f0
frakhmanzss <fauzi.rakhman@zensoftware.us>
2024-02-05 15:24:11 +0700

    Add geotag

--- zen_hr_attendance_extended/models/hr_work_location.py
+++ zen_hr_attendance_extended/models/hr_work_location.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class HrWorkLocation(models.Model):
@@ -6,4 +6,10 @@ class HrWorkLocation(models.Model):
 
     lat = fields.Float(string="Lattitude", digits=(10, 7))
     long = fields.Float(string="Longitude", digits=(10, 7))
+    geotag = fields.Char(string="Geotag", compute='_compute_geotag', store=True)
     address = fields.Text(string="Address")
+
+    @api.depends('lat','long')
+    def _compute_geotag(self):
+        for rec in self:
+            rec.geotag=f'{rec.lat},{rec.long}'
